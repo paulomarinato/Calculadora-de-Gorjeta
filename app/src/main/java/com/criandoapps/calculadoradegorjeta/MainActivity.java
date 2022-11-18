47418711440b06dcaf0789c2ca5c357012bc44f8
paulomarinato <marinatopaulo@gmail.com>
2022-11-18 17:52:02 -0300

    Finalizando Botões e configurações

--- app/src/main/java/com/criandoapps/calculadoradegorjeta/MainActivity.java
+++ app/src/main/java/com/criandoapps/calculadoradegorjeta/MainActivity.java
@@ -3,12 +3,43 @@ package com.criandoapps.calculadoradegorjeta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editValor;
+    private TextView textPorcentagem;
+    private TextView textGorjeta;
+    private TextView textTotal;
+    private SeekBar porcentGorjeta;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editValor        = findViewById(R.id.editValor);
+        textPorcentagem  = findViewById(R.id.textPorcentagem);
+        textGorjeta      = findViewById(R.id.textGorjeta);
+        textTotal        = findViewById(R.id.textTotal);
+        porcentGorjeta   = findViewById(R.id.porcentGorjeta);
+
+        porcentGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        }
     }
 }
